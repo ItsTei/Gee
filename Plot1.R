58c0b4ba2ef04e318c170daaf2d318d3982e5005
ItsTei <166406623+ItsTei@users.noreply.github.com>
2026-02-26 08:36:31 +0300

    Update and rename Plot1 to Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -8,11 +8,7 @@ png("plot1.png", width = 800, height = 500)
 totals <- tapply(NEI$Emissions, NEI$year, sum, na.rm = TRUE)
 years <- as.numeric(names(totals))
 
-plot(
-  years, as.numeric(totals),
-  type = "b",
-  xlab = "Year",
-  ylab = "Total PM2.5 Emissions (tons)",
+plot(  years, as.numeric(totals), type = "b", xlab = "Year", ylab = "Total PM2.5 Emissions (tons)",
   main = "Total PM2.5 Emissions in the United States (1999–2008)"
 )
 
