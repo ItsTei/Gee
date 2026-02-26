a0ab4364d8462de8535a35f54d762a6c26084448
ItsTei <166406623+ItsTei@users.noreply.github.com>
2026-02-26 08:49:04 +0300

    Update and rename PLOT2 to Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -10,11 +10,7 @@ png("plot2.png", width = 800, height = 500)
 totals <- tapply(balt$Emissions, balt$year, sum, na.rm = TRUE)
 years <- as.numeric(names(totals))
 
-plot(
-  years, as.numeric(totals),
-  type = "b",
-  xlab = "Year",
-  ylab = "Total PM2.5 Emissions (tons)",
+plot( years, as.numeric(totals), type = "b", xlab = "Year", ylab = "Total PM2.5 Emissions (tons)",
   main = "Total PM2.5 Emissions in Baltimore City (1999–2008)"
 )
 
